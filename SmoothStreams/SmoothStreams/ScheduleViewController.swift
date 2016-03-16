bbec1877be424e6db61e049b5da20001bc93b6bc
Omar Basrawi <omarbasrawi@gmail.com>
2016-03-16 15:56:02 -0400

    ScheduleViewController
    Create constants for segue strings
    Make helper functions private
    Implement ExtraOptionsDelegate and store incoming data
    Add qualityType to URL params
    Become part of AVAudioSessionCategoryPlayback

--- SmoothStreams/SmoothStreams/ScheduleViewController.swift
+++ SmoothStreams/SmoothStreams/ScheduleViewController.swift
@@ -12,16 +12,17 @@ import AVKit
 import Locksmith
 
 class ScheduleViewController: UITableViewController {
-
 	let ChannelCellIdentifier = "ChannelCellIdentifier"
+	let ScheduleToOptionsSegue = "OptionsSegue"
+	let ScheduleToPlayerSegue = "PlayRequestedChannelSegue"
+
+
 	var tempModel = [Int]()
 	let mode = Services()
+	var qualityType = LOW_QUALITY_STREAM
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-		self.navigationController?.title = "Schedule"
-
 		// Temporary fix until I parse the schedule JSON
 		for i in 1...50 {
 			tempModel.append(i)
@@ -31,47 +32,57 @@ class ScheduleViewController: UITableViewController {
     // MARK: - Navigation
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-		if segue.identifier == "PlayRequestedChannelSegue" {
+		if segue.identifier == ScheduleToOptionsSegue {
+
+			let navigationViewController = segue.destinationViewController as! UINavigationController
+			let extraOptionsViewController = navigationViewController.topViewController as! ExtraOptionsTableViewController
+
+			extraOptionsViewController.streamType = qualityType
+			extraOptionsViewController.delegate = self
+
+		} else if segue.identifier == ScheduleToPlayerSegue {
 			let playerViewController = segue.destinationViewController as! AVPlayerViewController
-			let path = tableView.indexPathForSelectedRow
-			let channel = adjustChannelID(tempModel[(path?.row)!])
+
+			guard let path = tableView.indexPathForSelectedRow?.row else { return }
+
+			let channel = adjustChannelID(tempModel[path])
 
 			getVideoURL(channel, success: { URL in
 
-				print(URL)
+				//print(URL)
 				playerViewController.player = AVPlayer(URL: URL)
-				playerViewController.player?.play()
 
+				do {
+					try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+
+				} catch { print("Could not play") }
+
+				playerViewController.modalPresentationStyle = UIModalPresentationStyle.FullScreen
+				playerViewController.player?.play()
 			})
 		}
 	}
 
 	// MARK: Helpers
 
-	func getVideoURL(channel: String, success: (NSURL) -> ()) {
+	private func getVideoURL(channel: String, success: (NSURL) -> ()) {
 		let prefs = NSUserDefaults.standardUserDefaults()
 		let s = prefs.valueForKey("site") as! Int
-		let a = mode.serviceForRow(s)
+		let a = mode.serviceDetailsForRow(s)
 		// FIXME: Introduce a option to select different hosts
 		let host = "deu.nl2.smoothstreams.tv"
 
 		getToken { token in
-			success(NSURL(string: "http://\(host):\(a.port)/\(a.host)/ch\(channel).smil/playlist.m3u8?wmsAuthSign=\(token)")!)
+			success(NSURL(string: "http://\(host):\(a.port)/\(a.host)/ch\(channel)\(self.qualityType).stream/playlist.m3u8?wmsAuthSign=\(token)")!)
 		}
 	}
 
-	func adjustChannelID(channel: Int) -> String {
-		if String(channel).characters.count == 1 {
-			return "0\(channel)"
-		}
-
-		return String(channel)
+	private func adjustChannelID(channel: Int) -> String {
+		return String(channel).characters.count == 1 ? "0\(channel)" : String(channel)
 	}
 
-	func getToken(token: (String) -> ()) {
-		if let dict = Locksmith.loadDataForUserAccount("ManagerAccount") {
+	private func getToken(token: (String) -> ()) {
+		if let dict = Locksmith.loadDataForUserAccount(AuthenticationManager.ManagerAccount) {
 			token(dict["hash"]! as! String)
 		}
 	}
@@ -79,7 +90,12 @@ class ScheduleViewController: UITableViewController {
 
 // TableView Methods
 
-extension ScheduleViewController {
+extension ScheduleViewController: ExtraOptionsDelegate {
+	func testingSomeDelegate(data: String) {
+		//Grab value from ExtraOptionsTableViewController via Delegate
+		self.qualityType = data
+	}
+
 	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		// This is temporary until I parse the schedule JSON
 		return 50
